de88db7396b7084a2023370bb425aaa14fe370d7
Abby <princessabigail133@gmail.com>
2024-09-20 07:55:28 +0100

    upd/navbar

--- src/layout/navbar/navbarIndex.tsx
+++ src/layout/navbar/navbarIndex.tsx
@@ -7,71 +7,57 @@ import { RavenButton } from "@ravenpay/raven-bank-ui";
 import { useOnClickOutside } from "../../helper/UseOnClickOutside";
 import { useState } from "react";
 import { linkList } from "./navbardata";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
   const [showDrop, setShowDrop] = useState("");
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+const navigate = useNavigate()
 
-  const handleMouseEnter = (index: number) => {
-    setActiveIndex(index); // Set the active index on mouse over
-  };
-
-  const handleMouseLeave = () => {
-    setActiveIndex(null); // Reset the active index on mouse leave
+  const handleClick = () => {
+    navigate('/blog'); 
+    window.scrollTo(0, 0);
   };
   const testRef = useOnClickOutside(() => {
     showDrop && setShowDrop("");
   });
   return (
-    
-      <div className="navbar-general-wrap-container-holder">
-        <div className="navbar-img-box">
-          <figure>{heroIcons.ravenLogo}</figure>
-        </div>
-        <div ref={testRef} className="navbar-item-container">
-          {linkList?.map((child, index) => {
-            const { name, icon, options, bigOption } = child;
-            return (
+    <div className="navbar-general-wrap-container-holder">
+      <div className="navbar-img-box">
+        <figure>{heroIcons.ravenLogo}</figure>
+      </div>
+      <div ref={testRef} className="navbar-item-container">
+        {linkList?.map((child, index) => {
+          const { name, icon, options, bigOption } = child;
+          return (
+            <div
+              onClick={() => {
+                name === showDrop ? setShowDrop("") : setShowDrop(name);
+              }}
+              key={index}
+              className="link-item"
+            >
+              <p className="name">{name}</p>
+              {icon && (
+                <>
+                  <figure className="img-box">{globalIcons.arrowDown}</figure>
+                </>
+              )}
               <div
-                onClick={() => {
-                  name === showDrop ? setShowDrop("") : setShowDrop(name);
-                }}
-                key={index}
-                className="link-item"
+                className={`drop-box-wrapper ${
+                  bigOption && "drop-box-wrapper-big"
+                }  ${name === showDrop && "drop-box-wrapper-anime"}`}
               >
-                <p className="name">{name}</p>
-                {icon && (
-                  <>
-                    <figure className="img-box">{globalIcons.arrowDown}</figure>
-                  </>
-                )}
-                <div 
-                className={
-                  `drop-box-wrapper ${
-                    bigOption && "drop-box-wrapper-big"
-                  }  ${name === showDrop && "drop-box-wrapper-anime"}`}
-                >
-                  <div 
-                  className="top-wrap"
-                  
-                  >
-{""}
-{options?.map((child, i) => {
-                    const { color, icon, label, description } = child;
+                <div className="top-wrap">
+                  {""}
+                  {options?.map((child, i) => {
+                    const { color, icon, label, description,link } = child;
                     return (
-                      <div 
-                      onMouseEnter={() => handleMouseEnter(i)}
-                      onMouseLeave={handleMouseLeave}
-                      className={`drop-box ${activeIndex === i ? 'active' : ''}`} // Add 'active' class on hover
-                      
-                      
-                      >
+                      <div key={i} 
+                      onClick={()=>{navigate(child.link)}}
+                      className="drop-box">
                         {/* icon box start */}
-                        <div
-                          
-                          className="icon-box"
-                        >
+                        <div className="icon-box">
                           <>
-                          <figure>{icon}</figure>
+                            <figure>{icon}</figure>
                           </>
                         </div>
                         {/* icon box end */}
@@ -82,36 +68,37 @@ const Navbar = () => {
                       </div>
                     );
                   })}
-                  </div>
-                  {bigOption && (
+                </div>
+                {bigOption && (
                   <>
                     <div className="blog-learn-box">
-                        <p className="text">Learn more about Raven Bank and Financial Management.</p>
+                      <p className="text">
+                        Learn more about Raven Bank and Financial Management.
+                      </p>
 
-                        <p className="link">Our Blog<span><figure>{navbarIcons.arowUp}</figure></span></p>
-
-                        
-                          
-                        
+                      <p className="link">
+                        Our Blog
+                        <span>
+                          <figure>{navbarIcons.arowUp}</figure>
+                        </span>
+                      </p>
                     </div>
                   </>
                 )}
-                </div>
               </div>
-            );
-          })}
-          
-        </div>
-        <div className="action-box">
-          <RavenButton
-            color="white-light"
-            label="Sign In"
-            textColor="deep-green-light"
-          />
-          <RavenButton color="green-light" label="Get Started" />
-        </div>
+            </div>
+          );
+        })}
       </div>
-    
+      <div className="action-box">
+        <RavenButton
+          color="white-light"
+          label="Sign In"
+          textColor="deep-green-light"
+        />
+        <RavenButton color="green-light" label="Get Started" />
+      </div>
+    </div>
   );
 };
 export default Navbar;
